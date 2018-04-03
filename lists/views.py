331d3eee2cfbb0dc3e278d868e81d8ffcf8e4c9b
BuildTools <unconfigured@null.spigotmc.org>
2018-04-03 16:10:43 +0900

    모델 검증을 위한 신규 목록 뷰 수정

--- lists/views.py
+++ lists/views.py
@@ -14,10 +14,12 @@ def view_list(request, list_id):
 
 def new_list(request):
 	list_ = List.objects.create()
-	item = Item.objects.create(text=request.POST['item_text'], list=list_)
+	item = Item(text=request.POST['item_text'], list=list_)
 	try:
 		item.full_clean() # if error occurs then it goes back to home page
+		item.save()
 	except ValidationError:
+		list_.delete()
 		error = "You can't have an empty list item"
 		return render(request, 'home.html', {"error":error})
 
