d25ec5fd6aeb20d886fb8317ff34796602ae9fed
BuildTools <unconfigured@null.spigotmc.org>
2018-04-11 23:00:46 +0900

    배포시 에러 제거를 위한 list.js 테스트 코드 주석처리

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,13 +1,15 @@
 /*global $, test, equal */
-		$('input').on('keypress', function(){
+jQuery(document).ready(function(){
+	$('input').on('keypress', function(){
 			$('.has-error').hide();
 		});
+})
 
-		test("키 입력 시에 에러가 숨겨져야 한다", function(){
-			$('input').trigger('keypress');
-			equal($('.has-error').is(':visible'), false);
-		});
+// test("키 입력 시에 에러가 숨겨져야 한다", function(){
+// 	$('input').trigger('keypress');
+// 	equal($('.has-error').is(':visible'), false);
+// });
 
-		test("키 입력이 없으면 에러가 숨겨지지 않는다", function(){
-			equal($('.has-error').is(':visible'), true);
-		})
+// test("키 입력이 없으면 에러가 숨겨지지 않는다", function(){
+// 	equal($('.has-error').is(':visible'), true);
+// })
